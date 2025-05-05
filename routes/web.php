9efef339d52bede58f772a2df69ef9357ac4a672
JohnDoeCode <guillermonestor2016@gmail.com>
2025-05-05 19:13:32 -0300

    Tareas To-Do-List (Terminar)

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+use App\Http\Controllers\TareaController;
+
+Route::get('/', [TareaController::class, 'index']);
+Route::post('/tareas', [TareaController::class, 'store']);
+Route::get('/tareas/{id}/edit', [TareaController::class, 'edit']);
+Route::put('/tareas/{id}', [TareaController::class, 'update']);
+Route::delete('/tareas/{id}', [TareaController::class, 'destroy']);
